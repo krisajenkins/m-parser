50c600ef15f724965179108c7dadd207f52d4078
Kris Jenkins <krisajenkins@gmail.com>
2012-08-20 11:52:20 +0100

    Updating the project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject m-parser "0.1.0"
   :description "A monadic parser for Clojure"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/krisajenkins/m-parser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
